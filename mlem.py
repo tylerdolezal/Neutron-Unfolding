8d1bc3a62e443cd97d82ee25f7bd346fd8f23cf4
Tyler Dolezal <47109396+tylerdolezal@users.noreply.github.com>
2021-08-19 21:05:33 -0400

    Update mlem.py

--- mlem.py
+++ mlem.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def mlem(R,data,x,steps):
-    n = len(data) # true is the recorded specturm
+def mlem(R,N,x,steps):
+    n = len(N) # true is the recorded specturm
     m = len(x)
 
     error = np.ones((steps+1,))
@@ -11,7 +11,7 @@ def mlem(R,data,x,steps):
         q = np.zeros((n,))
         for i in range(n):
             factor = (R[i,:]@x)
-            vector[i] = data[i]/factor
+            vector[i] = N[i]/factor
             q[i] = factor
 
         for j in range(m):
@@ -19,7 +19,7 @@ def mlem(R,data,x,steps):
             x[j] *= (1 / sum(R[:,j]))*term
 
 
-        J = sum((q-data)**2) / sum(q)
+        J = sum((q-N)**2) / sum(q)
         error[astep] = J
         print("Iteration {}, J = {}".format(astep,J))
 
